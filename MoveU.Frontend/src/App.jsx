8422854854e1fd6f59efa0d98371926ba56426e4
Mefisto051 <88355123+Mefisto051@users.noreply.github.com>
2025-11-22 13:45:55 -0500

    Update App.jsx

--- MoveU.Frontend/src/App.jsx
+++ MoveU.Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login';
 // import Register from './pages/Register';  // Lo agregaremos después
 // import Dashboard from './pages/Dashboard'; // Lo agregaremos después
-import './App.css';
+//import './App.css';
 
 function App() {
   return (

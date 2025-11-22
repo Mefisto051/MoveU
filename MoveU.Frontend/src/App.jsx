0cfb86f11ad38981f79e5996c51db0ace7732943
Mefisto051 <88355123+Mefisto051@users.noreply.github.com>
2025-11-22 18:34:56 -0500

    Update App.jsx

--- MoveU.Frontend/src/App.jsx
+++ MoveU.Frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import Dashboard from './pages/Dashboard'; // Lo agregaremos después
-//import './App.css';
+import Dashboard from './pages/Dashboard'; 
 
 function App() {
   return (
@@ -10,8 +9,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> 
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} /> 
           <Route path="/" element={<Navigate to="/login" replace />} />
         </Routes>
       </div>

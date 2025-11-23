80f2c33dbb1bb976393950313a7569065d429832
Mefisto051 <88355123+Mefisto051@users.noreply.github.com>
2025-11-22 22:12:25 -0500

    Update UserController.cs

--- MoveU.Api/Controllers/UserController.cs
+++ MoveU.Api/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MoveU.Application.Interfaces;
+using MoveU.Application.DTOs; // ✅ LÍNEA AGREGADA
 
 namespace MoveU.Api.Controllers
 {
